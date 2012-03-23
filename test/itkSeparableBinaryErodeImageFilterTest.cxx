39779647f1a0dd660964701f7811b2881f4fee43
Bradley Lowekamp <blowekamp@mail.nih.gov>
2012-03-23 11:28:06 -0400

    Adding initial test  for Separable binary morphology
    These tests a nearly a direct copy of the current binary morphology
    tests currently in ITK.

---
+++ test/itkSeparableBinaryErodeImageFilterTest.cxx
@@ -0,0 +1,74 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkSimpleFilterWatcher.h"
+#include "itkSeparableBinaryErodeImageFilter.h"
+
+int itkSeparableBinaryErodeImageFilterTest(int argc, char * argv[])
+{
+  if( argc < 7 )
+    {
+    std::cerr << "Missing Parameters " << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " InputImage OutputImage Foreground Background BoundaryToForeground Radius" << std::endl;
+    return EXIT_FAILURE;
+    }
+  const int dim = 2;
+
+  typedef unsigned char            PType;
+  typedef itk::Image< PType, dim > IType;
+
+  typedef itk::ImageFileReader< IType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+
+  typedef itk::FlatStructuringElement<dim > SRType;
+  typedef itk::SeparableBinaryErodeImageFilter< IType, IType, SRType > FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( reader->GetOutput() );
+  filter->SetRadius( atoi(argv[6]) );
+
+
+  itk::SimpleFilterWatcher watcher(filter, "filter");
+
+  typedef itk::ImageFileWriter< IType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( filter->GetOutput() );
+  writer->SetFileName( argv[2] );
+
+  filter->SetForegroundValue( atoi(argv[3]) );
+  filter->SetBackgroundValue( atoi(argv[4]) );
+  filter->SetBoundaryToForeground( atoi(argv[5]) );
+
+
+  try
+    {
+    writer->Update();
+    }
+  catch ( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+
+}
